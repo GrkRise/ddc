e23cc8797ce43c2c81cce97fa2ac8c69a4797bcb
Igor Razzhivaykin <grk.rise@gmail.com>
2015-02-10 22:18:55 +0300

    UI

---
+++ app/src/main/java/com/example/grk_rise/ddc/CameraFragment.java
@@ -0,0 +1,32 @@
+package com.example.grk_rise.ddc;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CameraFragment extends HomeActivity.PlaceholderFragment {
+
+
+    public CameraFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_camera,
+                container, false);
+        return rootView;
+    }
+
+
+}
